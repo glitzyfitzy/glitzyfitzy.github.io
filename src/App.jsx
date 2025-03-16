d49fd965982603340822b828d120211e01ee2a61
joseph lawler <joe-lawler-23@jobot-services.com>
2025-03-16 13:28:09 -0500

    Change for hayley

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from "react";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1 class="text-lg font-bold underline">Hello world! Minor change</h1>
+      <h1 class="text-lg font-bold underline">
+        Hey Hayley, ik this is basic but this is running for free on someone
+        elses computer so you shouldn't worry about it not being up
+      </h1>
     </>
   );
 }
