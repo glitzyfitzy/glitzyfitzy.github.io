114cd0a35e7d15ecee8cd6b1981e6fd783408db9
joseph lawler <joe-lawler-23@jobot-services.com>
2025-03-16 23:38:18 -0500

    Added sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { SocialMediaSection } from "./sections/SocialMedia";
+import { Timeline } from "./sections/Timeline";
+
 function App() {
   return (
     <>
-      <h1 class="text-lg font-bold underline">
+      <h1 className="text-lg font-bold underline">
         Hey Hayley, ik this is basic but this is running for free on someone
         elses computer so you shouldn't worry about it not being up
       </h1>
+      <SocialMediaSection />
+      <Timeline />
     </>
   );
 }
