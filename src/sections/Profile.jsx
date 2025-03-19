2ee09d68ce7d0def018ef7fc270af3fd8c394e84
joseph lawler <joe-lawler-23@jobot-services.com>
2025-03-19 18:33:08 -0500

    Fix for width on small devices

--- src/sections/Profile.jsx
+++ src/sections/Profile.jsx
@@ -2,8 +2,17 @@ import profile from "../assets/profile.jpg";
 
 export const Profile = () => {
   return (
-    <div className="flex justify-center align-middle">
-      <img src={profile} className="w-1/3 object-contain block" />
+    <div className="flex justify-center align-middle w-full">
+      <img
+        src={profile}
+        className="
+        max-w-xs
+        sm:max-w-md
+        md:max-w-lg
+        lg:max-w-lg
+        xl:max-w-lg
+        h-auto"
+      />
     </div>
   );
 };
